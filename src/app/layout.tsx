1d4c463a37f72d63f06477e27ee1365dd01d5efe
Pratchaya007 <pratchaya.1012@gmail.com>
2026-05-05 16:14:55 +0700

    feat: Add shadcn button and input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import { notoSans } from "@/style/font";
 import "@/style/globals.css";
 import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: {
@@ -15,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body className={`antialiased ${notoSans.className}`}>{children}</body>
     </html>
   );
